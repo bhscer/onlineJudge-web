7de15ba2b510c3bf44cb079ed8527a4f2dcc79e1
Bhscer <bhscer@163.com>
2023-05-02 23:04:12 +0800

    fix a login problem

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -93,11 +93,11 @@ export const useUserStore = defineStore('user', () => {
       })
       .catch((error) => {
         auth_ing.value = false;
-        localStorage.removeItem('oj-auth-token')
         let err_msg = '';
         try {
           if (error.response.status === 401 || error.response.status === 400) {
             err_msg = error.response.data.detail;
+            localStorage.removeItem('oj-auth-token')
           } else {
             err_msg = '错误:' + error.response.status;
           }
