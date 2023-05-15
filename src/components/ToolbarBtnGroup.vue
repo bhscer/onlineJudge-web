02c9eb6a069a8b2d9663ce930b3055472c825766
Bhscer <bhscer@163.com>
2023-05-15 18:57:51 +0800

    add fullscreen function

--- src/components/ToolbarBtnGroup.vue
+++ src/components/ToolbarBtnGroup.vue
@@ -3,6 +3,8 @@
 	<toolbar-add-btn
 		v-if="user.permission.Event.canWrite || user.permission.Blog.canWrite"
 	></toolbar-add-btn>-->
+
+  <q-btn icon="fullscreen" size="lg" flat rounded @click="changFullscreen" />
   <switch-theme-btn></switch-theme-btn>
 
   <account-btn></account-btn>
@@ -10,9 +12,37 @@
 
 <script setup lang="ts">
 // import { useUserStore } from '@/stores/user';
+import { useQuasar } from 'quasar';
 import AccountBtn from './btn/AccountBtn.vue';
 import SwitchThemeBtn from './btn/SwitchThemeBtn.vue';
 // import ToolbarAddBtn from "./btn/ToolbarAddBtn.vue";
 
 // const user = useUserStore();
+let fullscreen = false;
+const $q = useQuasar();
+
+function changFullscreen() {
+  if (!fullscreen) {
+    $q.fullscreen
+      .request()
+      .then(() => {
+        // success!
+        fullscreen = true;
+      })
+      .catch((err) => {
+        // oh, no!!!
+      });
+  } else {
+    // 退出全屏模式：
+    $q.fullscreen
+      .exit()
+      .then(() => {
+        // success!
+        fullscreen = false;
+      })
+      .catch((err) => {
+        // oh, no!!!
+      });
+  }
+}
 </script>
