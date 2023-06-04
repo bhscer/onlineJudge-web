e0a6fc6b1ebdccd2ed86dbd90695327c2fa867b7
bhscer <129813414+bhscer@users.noreply.github.com>
2023-06-04 23:05:00 +0800

    add background in loading component

--- src/components/loadingPage.vue
+++ src/components/loadingPage.vue
@@ -3,14 +3,17 @@
     :showing="props.loading && !message?.length"
     style="background-color: transparent"
   >
+  <q-card class="q-pa-lg">
+
     <q-spinner-gears size="50px" color="primary" />
-    <p>{{ loading_msg || 'loading...' }}</p>
+    <p class="q-ma-none q-pa-none">{{ loading_msg || 'loading...' }}</p>
+  </q-card>
   </q-inner-loading>
-  <div v-if="message?.length">
+  <q-card v-if="message?.length" class="q-pa-lg">
     <h2 style="padding: 0; margin: 0"><strong>:(</strong></h2>
     <h5 style="padding-top: 30px; margin: 0">出错了</h5>
-    <p>{{ props.message }}</p>
-  </div>
+    <p class="q-ma-none q-pa-none">{{ props.message }}</p>
+  </q-card>
 </template>
 
 <script setup lang="ts">
