ceb22f6e7ec05a2de25b2d64e0de85a1ea725e38
bhscer <129813414+bhscer@users.noreply.github.com>
2023-06-06 13:20:11 +0800

    fix:状态页卡片于顶端没有间隙

--- src/pages/menuPages/status.vue
+++ src/pages/menuPages/status.vue
@@ -3,7 +3,7 @@
     class="flex flex-center"
     style="flex-wrap: wrap; flex-direction: column"
   >
-    <div style="width: 1000px">
+    <div style="width: 1000px" class="q-mt-md">
       <submission-list></submission-list>
     </div>
   </q-page>
